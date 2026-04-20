038fdfada21dc1d0994204030cd9ec929d705ca5
c0utin <rafaelcouto111@gmail.com>
2026-04-20 17:46:05 -0300

    things

--- init.el
+++ init.el
@@ -4,3 +4,26 @@
 (column-number-mode 1)
 (global-display-line-numbers-mode)
 
+(set-face-attribute 'default nil :font "Iosevka Nerd Font Mono 20")
+
+(require 'package)
+
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/") t)
+
+(package-initialize)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(gruber-darker-theme magit)))
+
+(load-theme 'gruber-darker t)
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
